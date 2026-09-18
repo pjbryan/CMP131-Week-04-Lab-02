1f538be1b9ceb98bdd85a68215254b824a555c18
PJ Bryan <pjbryan2004@gmail.com>
2026-09-18 10:52:43 -0400

    working on part two, but part one should be done

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -4,3 +4,12 @@
 # Lab 02
 # Assignment 4
 # 09/18/2026
+
+programTitle = "Arithmetic Operations"
+divider = "___________________"
+
+firstNumber = "Enter first number:"
+secondNumber = "Enter second number:"
+
+userInput1 = (input(firstNumber))
+userInput2 = (input(secondNumber))
