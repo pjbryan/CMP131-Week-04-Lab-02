55a72ac28664d67881d3d781ca8667955fd5a0d9
PJ Bryan <pjbryan2004@gmail.com>
2026-09-23 11:15:53 -0400

    finished at part 6

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -5,11 +5,31 @@
 # Assignment 4
 # 09/18/2026
 
-programTitle = "Arithmetic Operations"
+programTitle = "ARITHMETIC OPERATIONS"
 divider = "___________________"
+divider2 = "*******************"
 
+print(programTitle, divider2)
+print("ENTER YOUR VALUES", divider)
 firstNumber = "Enter first number:"
 secondNumber = "Enter second number:"
 
-userInput1 = (input(firstNumber))
-userInput2 = (input(secondNumber))
+userInput1 = float(input(firstNumber))
+userInput2 = float(input(secondNumber))
+
+addition = userInput1 + userInput2
+subtraction = userInput1 - userInput2
+multiplication = userInput1 * userInput2
+division = userInput1 / userInput2
+# power =
+# average =
+
+print()
+print("OUTPUT", divider)
+
+print("Addition:", addition)
+print("Subtraction:",subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Power:")
+print("Average:")
